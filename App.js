dc92509f4a46d72e29028b18d631fc68262f3bb9
RAJATH R RAO <rajath642000@gmail.com>
2021-07-07 14:56:14 +0530

    commit 1

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import home from "./screens/home";
+import ToDoList from "./screens/toDoList";
+import EditList from "./screens/editList";
+import Colors from "./constants/colors";
+import firebase from "firebase";
+import Login from "./screens/Login";
+import settings from './screens/settings';
 
-export default function App() {
+const Stack = createStackNavigator();
+const AuthStack = createStackNavigator();
+
+const AuthScreens = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthStack.Navigator>
+      <AuthStack.Screen name="Login" component={Login}/>
+    </AuthStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Screens = () => {
+  return (
+    <Stack.Navigator>
+          <Stack.Screen name="Fire Todo" component={home}/>
+          <Stack.Screen name="Settings" component={settings}/>
+          <Stack.Screen name="ToDO List" component={ToDoList}
+          options={({route})=> {
+            return ({
+              title: route.params.title,
+              headerStyle: {
+                backgroundColor: route.params.color,
+              },
+              headerTintColor: "white",
+            });
+          }}/>
+          <Stack.Screen name="Edit List" component={EditList}
+          options={({route})=> {
+            return ({
+              title: route.params.title? `Edit ${route.params.title} List`: "New List",
+              headerStyle: {
+                backgroundColor: route.params.color || Colors.blue,
+              },
+              headerTintColor: "white",
+            });
+          }}/>
+      </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(()=> {
+    if(firebase.auth().currentUser) {
+      setIsAuthenticated(true);
+    }
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    });
+  },[]);
+  return (
+    <NavigationContainer>
+      {isAuthenticated?<Screens/>:<AuthScreens/>}
+    </NavigationContainer>
+  );
+}
+
+const firebaseConfig = {
+  apiKey: "abc",
+  authDomain: "abc",
+  projectId: "abc",
+  storageBucket: "abc",
+  messagingSenderId: "abc",
+  appId: "abc"
+};
+
+firebase.initializeApp(firebaseConfig);
